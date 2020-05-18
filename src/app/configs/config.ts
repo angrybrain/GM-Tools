7fbc8ce3cefa8ceb80899231a8f482ce6b6a7b7e
angrybrain <56646202+angrybrain@users.noreply.github.com>
2020-05-18 18:18:40 +0300

    1

--- src/app/configs/config.ts
+++ src/app/configs/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    apiUrl: 'https://gm-tools-api.herokuapp.com:3000',
-    socketUrl: 'https://gm-tools-api.herokuapp.com:4000',
+    apiUrl: 'https://gm-tools-api.herokuapp.com',
+    socketUrl: 'https://gm-tools-api.herokuapp.com',
 };
